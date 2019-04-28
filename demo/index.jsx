5d0d4ef156c545b5206e9e1cc3aba12b0a064ebc
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2019-04-28 17:43:20 +0300

    Setup demo

---
+++ demo/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ReactSearchInput from "../src/ReactSearchInput";
+
+ReactDOM.render(<ReactSearchInput />, document.getElementById("demo"));
