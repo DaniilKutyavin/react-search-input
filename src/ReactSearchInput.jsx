da9b685246c0fb17cf651a68a14c6c622220dda5
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2019-04-28 23:36:15 +0300

    Update RSI logic

--- src/ReactSearchInput.jsx
+++ src/ReactSearchInput.jsx
@@ -2,17 +2,24 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import Input from "./components/Input";
 import Result from "./components/Result";
+import "./style.css";
+
+const MIN_AMOUNT_TO_SHOW = 1;
 
 class ReactSearchInput extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       value: "",
-      result: props.data
+      result: props.data,
+      resultVisible: false
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleResultItemClick = this.handleResultItemClick.bind(this);
+    this.showResult = this.showResult.bind(this);
+    this.hideResult = this.hideResult.bind(this);
+    this.updateResultValues = this.updateResultValues.bind(this);
   }
 
   // eslint-disable-next-line no-unused-vars
@@ -21,14 +28,7 @@ class ReactSearchInput extends PureComponent {
 
     if (value !== prevState.value) {
       // filter dataset and update result list
-      const { data } = this.props;
-      const newResult = data.filter(datasetItem =>
-        datasetItem.toLowerCase().includes(value.toLowerCase())
-      );
-
-      // TODO figure out, how to remove this one
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({ result: newResult });
+      this.updateResultValues(value);
     }
   }
 
@@ -40,21 +40,50 @@ class ReactSearchInput extends PureComponent {
   }
 
   handleResultItemClick(value) {
-    return function handleClick() {
+    return () => {
       this.setState({ value });
-    }.bind(this);
+    };
+  }
+
+  showResult() {
+    this.setState({ resultVisible: true });
+  }
+
+  hideResult() {
+    setTimeout(() => {
+      this.setState({ resultVisible: false });
+    }, 150);
+  }
+
+  updateResultValues(inputValue) {
+    const { data } = this.props;
+
+    const newResult = data.filter(datasetItem => {
+      return datasetItem.toUpperCase().includes(inputValue.toUpperCase());
+    });
+
+    this.setState({ result: newResult });
   }
 
   render() {
-    const { value, result } = this.state;
+    const { value, result, resultVisible } = this.state;
+
+    const shouldShowResult =
+      resultVisible && result.length >= MIN_AMOUNT_TO_SHOW;
 
     return (
-      <div id="react-search-input">
+      <div
+        id="react-search-input"
+        onFocus={this.showResult}
+        onBlur={this.hideResult}
+      >
         <Input value={value} onChange={this.handleChange} />
-        <Result
-          result={result}
-          handleResultItemClick={this.handleResultItemClick}
-        />
+        {shouldShowResult && (
+          <Result
+            result={result}
+            handleResultItemClick={this.handleResultItemClick}
+          />
+        )}
       </div>
     );
   }
