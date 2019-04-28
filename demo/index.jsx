b748c216f9289bd3c908913ea933f906045ffdd0
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2019-04-28 18:24:16 +0300

    Update demo

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import data from "./dataset.json";
+
 import ReactSearchInput from "../src/ReactSearchInput";
 
-ReactDOM.render(<ReactSearchInput />, document.getElementById("demo"));
+ReactDOM.render(
+  <ReactSearchInput data={data} />,
+  document.getElementById("demo")
+);
