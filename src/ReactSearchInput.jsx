0f9adc668565c2323eba985734798ff14cf24d9f
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2019-04-28 18:00:56 +0300

    Add basic functions to Input

--- src/ReactSearchInput.jsx
+++ src/ReactSearchInput.jsx
@@ -5,13 +5,23 @@ import Results from "./components/Results";
 export default class ReactSearchInput extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      value: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.currentTarget.value });
   }
 
   render() {
+    const { value } = this.state;
+
     return (
       <div id="react-search-input">
-        <Input />
+        <Input value={value} onChange={this.handleChange} />
         <Results />
       </div>
     );
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Input() {
-  return <div />;
+export default function Input({ value = "", onChange }) {
+  return <input value={value} onChange={onChange} />;
 }
+
+Input.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
