79dc66775b1935e8ba146e0c8ee3d956dcd383c4
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2019-04-28 19:20:17 +0300

    Add value autofill on result item click

--- src/ReactSearchInput.jsx
+++ src/ReactSearchInput.jsx
@@ -12,19 +12,37 @@ class ReactSearchInput extends PureComponent {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleResultItemClick = this.handleResultItemClick.bind(this);
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  componentDidUpdate(_prevProps, prevState) {
+    const { value } = this.state;
+
+    if (value !== prevState.value) {
+      // filter dataset and update result list
+      const { data } = this.props;
+      const newResult = data.filter(datasetItem =>
+        datasetItem.toLowerCase().includes(value.toLowerCase())
+      );
+
+      // TODO figure out, how to remove this one
+      // eslint-disable-next-line react/no-did-update-set-state
+      this.setState({ result: newResult });
+    }
   }
 
   handleChange(e) {
     const inputValue = e.currentTarget.value;
-    const { data } = this.props;
-    const newResult = data.filter(datasetItem =>
-      datasetItem.toLowerCase().includes(inputValue.toLowerCase())
-    );
-
     this.setState({
-      value: inputValue,
-      result: newResult
+      value: inputValue
     });
+  }
+
+  handleResultItemClick(value) {
+    return function handleClick() {
+      this.setState({ value });
+    }.bind(this);
   }
 
   render() {
@@ -33,7 +51,10 @@ class ReactSearchInput extends PureComponent {
     return (
       <div id="react-search-input">
         <Input value={value} onChange={this.handleChange} />
-        <Result result={result} />
+        <Result
+          result={result}
+          handleResultItemClick={this.handleResultItemClick}
+        />
       </div>
     );
   }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function ResultListItem(props) {
-  return <div className="result-item" {...props} />;
+function ResultListItem({ value, handleResultItemClick, index }) {
+  return (
+    // TODO
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+    <div
+      className="result-item"
+      // TODO
+      // eslint-disable-next-line jsx-a11y/role-has-required-aria-props
+      role="option"
+      tabIndex={index}
+      onClick={handleResultItemClick(value)}
+    >
+      {value}
+    </div>
+  );
 }
 
-export default function Result({ result = [] }) {
-  return result.map(resultListItem => (
-    <ResultListItem id="result" key={resultListItem}>
-      {resultListItem}
-    </ResultListItem>
+export default function Result({ result = [], handleResultItemClick }) {
+  return result.map((resultListItem, index) => (
+    <ResultListItem
+      id="result"
+      key={resultListItem}
+      handleResultItemClick={handleResultItemClick}
+      value={resultListItem}
+      index={index}
+    />
   ));
 }
 
 Result.propTypes = {
   result: PropTypes.arrayOf(PropTypes.any).isRequired
+};
+
+ResultListItem.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleResultItemClick: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired
 };
